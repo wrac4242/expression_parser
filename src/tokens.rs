504c4812e02cec8216627bbbe25783ff7595a39b
Wrac4242 <wrac4242@gmail.com>
2022-08-28 12:59:24 +0100

    move tokenize into tokens

--- src/tokens.rs
+++ src/tokens.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub struct Token {
 	token_type: Tokens
 }
@@ -23,4 +25,24 @@ impl Token {
 	pub fn get_type(&self) -> Tokens {
 		self.token_type
 	}
+}
+
+// supports + - * / ()
+pub fn tokenize(to_tokenise: String) -> Vec<Token> {
+	// create iterator somehow over the string, match them into their syntaxes? 
+	let mut token_stream = Vec::new();
+	
+	for mat in Regex::new(r"\d+|[+*-/\(\)]").unwrap().find_iter(&to_tokenise) {
+		match mat.as_str() {
+			"+" => token_stream.push(Token::new(Tokens::Plus)),
+			"-" => token_stream.push(Token::new(Tokens::Minus)),
+			"*" => token_stream.push(Token::new(Tokens::Multiply)),
+			"/" => token_stream.push(Token::new(Tokens::Divide)),
+			"(" => token_stream.push(Token::new(Tokens::OpenBracket)),
+			")" => token_stream.push(Token::new(Tokens::CloseBracket)),
+			_ => token_stream.push(Token::new(Tokens::Number(mat.as_str().parse().unwrap()))) // assumes int
+		}
+	}
+
+	token_stream
 }
